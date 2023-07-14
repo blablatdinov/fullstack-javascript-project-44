53be8d9ec497b049c2110542b6fc3424f8ce45a5
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2023-07-14 17:48:55 +0300

    Third step

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const name = readlineSync.question('May I have your name? ');
 console.log('Welcome to the Brain Games!')
+console.log(`Hello, ${name}!`)
