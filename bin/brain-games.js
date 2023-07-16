77c1b412daa06da0888ba2cdb4dffe80d2ba9006
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2023-07-16 20:08:32 +0300

    Fix name

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,36 +1,7 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
-import name from '../index.js';
+import readName from './cli.js';
 
-const isPrime = (number) => {
-  if (number < 2) {
-    return false;
-  }
-  for (let i = 2; i <= Math.sqrt(number); i += 1) {
-    if (number % i === 0) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const primeGame = (playerName) => {
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  for (let i = 0; i < 3; i += 1) {
-    const number = Math.floor(Math.random() * 100);
-    const correctAnswer = isPrime(number) ? 'yes' : 'no';
-    console.log(`Question: ${number}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (correctAnswer === answer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${playerName}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${playerName}!`);
-};
-
-primeGame(name);
+const name = readName();
+console.log('Welcome to the Brain Games!');
+console.log(`Hello, ${name}!`);
